ce62be231833d889fcb9099e219d6ec376e2b3e6
Satyam <60954779+vsatyamesc@users.noreply.github.com>
2024-09-22 17:37:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
 var app = express();
 
+const upload = multer({ dest: "uploads/" });
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyze", upload.single("upfile"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
 
+  const { originalname, mimetype, size } = req.file;
 
+  res.json({
+    fileName: originalname,
+    type: mimetype,
+    size: size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
